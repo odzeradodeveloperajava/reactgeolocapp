153844421b214acdfe111cf6e72e8c4657f3e6b2
odzeradodeveloperajava <44056132+odzeradodeveloperajava@users.noreply.github.com>
2020-08-28 18:02:33 +0200

    Update App.js
    Fully functional

--- src/App.js
+++ src/App.js
@@ -4,49 +4,84 @@ import './App.css';
 import Header from './components/header/header';
 import UploadHandler from './components/upoloadHandler/uploadHandler';
 import CardsWrapper from './components/cardsWrapper/cardsWrapper';
-import fileHandler from './components/upoloadHandler/fileHandler';
+import fileHandler from './components/upoloadHandler/fileHandler.js';
 /*global EXIF*/
+const initialStateItems = [];
 
-class App extends React.Component {
-    state = {
-      items: [],
-    }
-    addItem = async () => {
-      const uploadedFiles = document.getElementById("input").files;
+class App extends React.Component{
+  state = {
+    items: [],
+  }
 
-      function uploadedfile(id, callback){
-          EXIF.getData(uploadedFiles, function() {
-                let object = {
-                  cardId: uploadedFiles[id].name,
-                  imageUrl: window.URL.createObjectURL(uploadedFiles[id]), // Create url for thumbnail of image //
-                  size: uploadedFiles[id].size
-                  };
-                
-                callback (object);
-              }
-          );
-    };
+addItem = e => {
 
-    let data = await uploadedfile(0);
-    console.log(data)
+  fileHandler();
+      let newItem = [];
+      let filesArray = document.getElementById('input').files.length;
+      //Loop for every file uploaded //
+      for (let i = 0; i < filesArray; i++) {
+        const selectedFile = document.getElementById('input').files[i];
+        EXIF.getData(selectedFile, function () {
+            if (this.exifdata.GPSLatitude !== undefined && this.exifdata.GPSLongitude !== undefined) {
+            
 
+              function ConvertDMSToDD(degrees, minutes, seconds, direction) {
+                var dd = degrees + (minutes/60) + (seconds/3600);
+                    if (direction === "S" || direction === "W") {
+                    dd = dd * -1;
+                }
+                    return dd;
+            };
+            
+            
+            function calculateGpsDatalat(){
+                // calculate latitude decimal
+                var latDegree = selectedFile.exifdata.GPSLatitude[0].numerator;
+                var latMinute = selectedFile.exifdata.GPSLatitude[1].numerator;
+                var latSecond = selectedFile.exifdata.GPSLatitude[2].numerator;
+                var latDirection = selectedFile.exifdata.GPSLatitudeRef;
+                return ConvertDMSToDD(latDegree, latMinute, latSecond, latDirection);
+            }
+            function calculateGpsDatalon(){
+                // calculate longitude decimal
+                var lonDegree = selectedFile.exifdata.GPSLongitude[0].numerator;
+                var lonMinute = selectedFile.exifdata.GPSLongitude[1].numerator;
+                var lonSecond = selectedFile.exifdata.GPSLongitude[2].numerator;
+                var lonDirection = selectedFile.exifdata.GPSLongitudeRef;
+                return  ConvertDMSToDD(lonDegree, lonMinute, lonSecond, lonDirection);
+            }
+
+              newItem.push({
+                cardId: this.name,
+                imageUrl: window.URL.createObjectURL(this), // Create url for thumbnail of image //
+                size: this.size,
+                lat: calculateGpsDatalat(),
+                lon: calculateGpsDatalon(),
+              })
+            } else {
+              console.log('error');
+            };
+        })
+      }
+
+  setTimeout(() => {
     this.setState(prevState => ({
-      items: [...prevState.items]
+      items: [...prevState.items, ...newItem]
     }));
-
-
+   }, 2000);
 };
-    render(){
-      return (
-        <div className="pageWrapper">
-          <Header />
-          <Map />
-          <UploadHandler submitFn={this.addItem} />
-          <CardsWrapper
-              items={this.state.items}
-          />
-        </div>
-      )};
-    }
+
+render(){
+  return (
+    <div className="pageWrapper">
+      <Header />
+      <Map />
+      <UploadHandler submitFn={this.addItem} />
+      <CardsWrapper
+          items={this.state.items}
+      />
+    </div>
+  )};
+}
 export default App;
 
