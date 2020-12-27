3db4b2408af438ce21c7ed91dcd46ee552afc813
odzeradodeveloperajava <44056132+odzeradodeveloperajava@users.noreply.github.com>
2020-12-27 13:26:16 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+
 import React from 'react';
 import Map from './components/map/map';
 import './App.css';
@@ -5,8 +6,9 @@ import Header from './components/header/header';
 import UploadHandler from './components/upoloadHandler/uploadHandler';
 import CardsWrapper from './components/cardsWrapper/cardsWrapper';
 import fileHandler from './components/upoloadHandler/fileHandler.js';
+import calculateGpsDatalon from './components/upoloadHandler/scripts/calculateGpsDatalon.js';
+import calculateGpsDatalat from './components/upoloadHandler/scripts/calculateGpsDatalat.js';
 /*global EXIF*/
-const initialStateItems = [];
 
 class App extends React.Component{
   state = {
@@ -23,40 +25,12 @@ addItem = e => {
         const selectedFile = document.getElementById('input').files[i];
         EXIF.getData(selectedFile, function () {
             if (this.exifdata.GPSLatitude !== undefined && this.exifdata.GPSLongitude !== undefined) {
-            
-
-              function ConvertDMSToDD(degrees, minutes, seconds, direction) {
-                var dd = degrees + (minutes/60) + (seconds/3600);
-                    if (direction === "S" || direction === "W") {
-                    dd = dd * -1;
-                }
-                    return dd;
-            };
-            
-            
-            function calculateGpsDatalat(){
-                // calculate latitude decimal
-                var latDegree = selectedFile.exifdata.GPSLatitude[0].numerator;
-                var latMinute = selectedFile.exifdata.GPSLatitude[1].numerator;
-                var latSecond = selectedFile.exifdata.GPSLatitude[2].numerator;
-                var latDirection = selectedFile.exifdata.GPSLatitudeRef;
-                return ConvertDMSToDD(latDegree, latMinute, latSecond, latDirection);
-            }
-            function calculateGpsDatalon(){
-                // calculate longitude decimal
-                var lonDegree = selectedFile.exifdata.GPSLongitude[0].numerator;
-                var lonMinute = selectedFile.exifdata.GPSLongitude[1].numerator;
-                var lonSecond = selectedFile.exifdata.GPSLongitude[2].numerator;
-                var lonDirection = selectedFile.exifdata.GPSLongitudeRef;
-                return  ConvertDMSToDD(lonDegree, lonMinute, lonSecond, lonDirection);
-            }
-
               newItem.push({
                 cardId: this.name,
                 imageUrl: window.URL.createObjectURL(this), // Create url for thumbnail of image //
                 size: this.size,
-                lat: calculateGpsDatalat(),
-                lon: calculateGpsDatalon(),
+                lat: calculateGpsDatalat(selectedFile),
+                lon: calculateGpsDatalon(selectedFile),
               })
             } else {
               console.log('error');
@@ -84,4 +58,3 @@ render(){
   )};
 }
 export default App;
-
